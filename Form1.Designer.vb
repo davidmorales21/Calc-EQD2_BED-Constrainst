56f480c48bab2c90566f85553433c861019a0936
davidmorales21 <102339662+davidmorales21@users.noreply.github.com>
2025-08-04 11:07:18 -0500

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,260 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.cmbOrganos = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtDosisFisica = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtValorConstraint = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtEQD2Organo = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtBEDOrgano = New System.Windows.Forms.TextBox()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtSesiones = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.txtAlphaBeta = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.txtEQD2Calculado = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.txtBEDCalculado = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'cmbOrganos
+        '
+        Me.cmbOrganos.FormattingEnabled = True
+        Me.cmbOrganos.Location = New System.Drawing.Point(162, 57)
+        Me.cmbOrganos.Name = "cmbOrganos"
+        Me.cmbOrganos.Size = New System.Drawing.Size(155, 21)
+        Me.cmbOrganos.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(159, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(54, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "ÓRGANO"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(46, 119)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(79, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "DOSIS FISICA:"
+        '
+        'txtDosisFisica
+        '
+        Me.txtDosisFisica.Location = New System.Drawing.Point(162, 112)
+        Me.txtDosisFisica.Name = "txtDosisFisica"
+        Me.txtDosisFisica.Size = New System.Drawing.Size(100, 20)
+        Me.txtDosisFisica.TabIndex = 3
+        Me.txtDosisFisica.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(359, 31)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(99, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "CONSTRAINT (Gy)"
+        '
+        'txtValorConstraint
+        '
+        Me.txtValorConstraint.Location = New System.Drawing.Point(358, 57)
+        Me.txtValorConstraint.Name = "txtValorConstraint"
+        Me.txtValorConstraint.Size = New System.Drawing.Size(100, 20)
+        Me.txtValorConstraint.TabIndex = 5
+        Me.txtValorConstraint.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(490, 31)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(58, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "EQD2 (Gy)"
+        '
+        'txtEQD2Organo
+        '
+        Me.txtEQD2Organo.Location = New System.Drawing.Point(483, 58)
+        Me.txtEQD2Organo.Name = "txtEQD2Organo"
+        Me.txtEQD2Organo.Size = New System.Drawing.Size(74, 20)
+        Me.txtEQD2Organo.TabIndex = 7
+        Me.txtEQD2Organo.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(598, 31)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(51, 13)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "BED (Gy)"
+        '
+        'txtBEDOrgano
+        '
+        Me.txtBEDOrgano.Location = New System.Drawing.Point(582, 58)
+        Me.txtBEDOrgano.Name = "txtBEDOrgano"
+        Me.txtBEDOrgano.Size = New System.Drawing.Size(80, 20)
+        Me.txtBEDOrgano.TabIndex = 9
+        Me.txtBEDOrgano.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.Location = New System.Drawing.Point(162, 226)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalcular.TabIndex = 10
+        Me.btnCalcular.Text = "CALCULAR"
+        Me.btnCalcular.UseVisualStyleBackColor = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(45, 152)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(111, 13)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "N° DE FRACCIONES:"
+        '
+        'txtSesiones
+        '
+        Me.txtSesiones.Location = New System.Drawing.Point(162, 145)
+        Me.txtSesiones.Name = "txtSesiones"
+        Me.txtSesiones.Size = New System.Drawing.Size(100, 20)
+        Me.txtSesiones.TabIndex = 12
+        Me.txtSesiones.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(46, 188)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(69, 13)
+        Me.Label7.TabIndex = 13
+        Me.Label7.Text = "ALFA/BETA:"
+        '
+        'txtAlphaBeta
+        '
+        Me.txtAlphaBeta.Location = New System.Drawing.Point(162, 181)
+        Me.txtAlphaBeta.Name = "txtAlphaBeta"
+        Me.txtAlphaBeta.Size = New System.Drawing.Size(100, 20)
+        Me.txtAlphaBeta.TabIndex = 14
+        Me.txtAlphaBeta.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(482, 93)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(88, 13)
+        Me.Label8.TabIndex = 15
+        Me.Label8.Text = "EQD2 CALC (Gy)"
+        '
+        'txtEQD2Calculado
+        '
+        Me.txtEQD2Calculado.Location = New System.Drawing.Point(483, 116)
+        Me.txtEQD2Calculado.Name = "txtEQD2Calculado"
+        Me.txtEQD2Calculado.Size = New System.Drawing.Size(74, 20)
+        Me.txtEQD2Calculado.TabIndex = 16
+        Me.txtEQD2Calculado.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(590, 93)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(81, 13)
+        Me.Label9.TabIndex = 17
+        Me.Label9.Text = "BED CALC (Gy)"
+        '
+        'txtBEDCalculado
+        '
+        Me.txtBEDCalculado.Location = New System.Drawing.Point(582, 116)
+        Me.txtBEDCalculado.Name = "txtBEDCalculado"
+        Me.txtBEDCalculado.Size = New System.Drawing.Size(80, 20)
+        Me.txtBEDCalculado.TabIndex = 18
+        Me.txtBEDCalculado.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(709, 301)
+        Me.Controls.Add(Me.txtBEDCalculado)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.txtEQD2Calculado)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.txtAlphaBeta)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.txtSesiones)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.btnCalcular)
+        Me.Controls.Add(Me.txtBEDOrgano)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtEQD2Organo)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtValorConstraint)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtDosisFisica)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cmbOrganos)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Calculadora dosis física a EQD2/BED"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents cmbOrganos As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtDosisFisica As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtValorConstraint As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtEQD2Organo As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtBEDOrgano As TextBox
+    Friend WithEvents btnCalcular As Button
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtSesiones As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents txtAlphaBeta As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents txtEQD2Calculado As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents txtBEDCalculado As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,177 @@
+﻿Public Class Form1
+    ' Evento que se ejecuta cuando el formulario se carga
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Añadir algunos órganos de ejemplo al ComboBox
+        cmbOrganos.Items.Add("Cerebro/Dmax<60")
+        cmbOrganos.Items.Add("Cerebro/Dmax=72")
+        cmbOrganos.Items.Add("Brainstem/dmax<54")
+        cmbOrganos.Items.Add("Brainstem/D0.1cc<=59")
+        cmbOrganos.Items.Add("Med.espinal/D0.1cc<=50")
+        cmbOrganos.Items.Add("Nerv.Optico/D0.1cc<=54")
+        cmbOrganos.Items.Add("Quisma.Optico/D0.1cc<=54")
+        cmbOrganos.Items.Add("Ojos/Dmax<54")
+        cmbOrganos.Items.Add("Ojos/Dmedia<=35")
+        cmbOrganos.Items.Add("Cristalino/D0.1cc<=10")
+        cmbOrganos.Items.Add("Coclea/Dmedia<=45")
+        cmbOrganos.Items.Add("Coclea/Dmedia<50")
+        cmbOrganos.Items.Add("Parotida(s)/Dmedia<=26")
+        cmbOrganos.Items.Add("Coclea/Dmedia<=45")
+        cmbOrganos.Items.Add("Tiroides/Dmedia<=45")
+        cmbOrganos.Items.Add("Laringe/Dmedia<=45")
+        cmbOrganos.Items.Add("Laringe/Dmax<66")
+        cmbOrganos.Items.Add("Esófago/Dmedia<34")
+        cmbOrganos.Items.Add("Esófago/V40%<50")
+        cmbOrganos.Items.Add("Mandibula/D0.1cc<=70")
+        cmbOrganos.Items.Add("Cavidad oral/V73%<30")
+        cmbOrganos.Items.Add("Labios/Dmedia<=50")
+        cmbOrganos.Items.Add("Gland.Pituitaria/Dmax<=50")
+        cmbOrganos.Items.Add("Pulmones/V30%<20")
+        cmbOrganos.Items.Add("Corazón/Dmedia<26")
+        cmbOrganos.Items.Add("Corazón/V30%<46")
+        cmbOrganos.Items.Add("Liver/Dmedia<=32")
+        cmbOrganos.Items.Add("Riñones/Dmedia<=18")
+        cmbOrganos.Items.Add("Estomago/Dmedia<=45")
+        cmbOrganos.Items.Add("Intestino/D195cc<=45")
+        cmbOrganos.Items.Add("Recto/V50%<=50")
+        cmbOrganos.Items.Add("Vejiga/V50%<=65")
+        cmbOrganos.Items.Add("Cabez.Femoral/Dmax<=54")
+        cmbOrganos.Items.Add("Cabez.Femoral/V10%<=50")
+
+        ' Configurar colores del formulario y controles
+        ConfigurarColores()
+    End Sub
+
+    ' Función para configurar los colores del formulario y controles
+    Private Sub ConfigurarColores()
+        ' Fondo del formulario
+        Me.BackColor = Color.FromArgb(45, 45, 45) ' Fondo oscuro
+        Me.ForeColor = Color.White ' Texto blanco
+
+        ' Configurar colores del ComboBox
+        cmbOrganos.BackColor = Color.FromArgb(60, 60, 60)
+        cmbOrganos.ForeColor = Color.White
+
+        ' Configurar colores de los TextBox
+        txtValorConstraint.BackColor = Color.FromArgb(60, 60, 60)
+        txtValorConstraint.ForeColor = Color.White
+        txtAlphaBeta.BackColor = Color.FromArgb(60, 60, 60)
+        txtAlphaBeta.ForeColor = Color.White
+        txtEQD2Organo.BackColor = Color.FromArgb(60, 60, 60)
+        txtEQD2Organo.ForeColor = Color.White
+        txtBEDOrgano.BackColor = Color.FromArgb(60, 60, 60)
+        txtBEDOrgano.ForeColor = Color.White
+        txtDosisFisica.BackColor = Color.FromArgb(60, 60, 60)
+        txtDosisFisica.ForeColor = Color.White
+        txtSesiones.BackColor = Color.FromArgb(60, 60, 60)
+        txtSesiones.ForeColor = Color.White
+        txtEQD2Calculado.BackColor = Color.FromArgb(60, 60, 60)
+        txtEQD2Calculado.ForeColor = Color.White
+        txtBEDCalculado.BackColor = Color.FromArgb(60, 60, 60)
+        txtBEDCalculado.ForeColor = Color.White
+
+        ' Configurar colores del botón
+        btnCalcular.BackColor = Color.FromArgb(0, 120, 215) ' Azul
+        btnCalcular.ForeColor = Color.White
+        btnCalcular.FlatStyle = FlatStyle.Flat
+        btnCalcular.FlatAppearance.BorderSize = 0
+
+        ' Configurar colores de las etiquetas (Labels)
+        For Each ctrl As Control In Me.Controls
+            If TypeOf ctrl Is Label Then
+                ctrl.ForeColor = Color.White
+            End If
+        Next
+    End Sub
+
+    ' Evento que se ejecuta cuando se selecciona un órgano en el ComboBox
+    Private Sub cmbOrganos_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbOrganos.SelectedIndexChanged
+        ' Extraer el valor de "Constraint" de la descripción del órgano seleccionado
+        Dim selectedOrgan As String = cmbOrganos.SelectedItem.ToString()
+        Dim constraintValue As Double = ExtraerConstraint(selectedOrgan)
+
+        ' Mostrar el valor de "Constraint" en el TextBox correspondiente
+        txtValorConstraint.Text = constraintValue.ToString()
+
+        ' Calcular EQD2 y BED del órgano basado en el valor de alfa/beta ingresado
+        CalcularEQD2yBEDOrgano()
+    End Sub
+
+    ' Función para extraer el valor de "Constraint" de la descripción del órgano
+    Private Function ExtraerConstraint(organDescription As String) As Double
+        ' Buscar el valor de "Constraint" en la descripción (por ejemplo, "dmax=50", "v30%<20", "dmax>=55")
+        Dim constraintPart As String = organDescription.Split("/"c)(1) ' Obtener la parte después de "/"
+        Dim constraintValue As Double
+
+        ' Extraer el número del texto, independientemente del operador (<, >, <=, >=, =)
+        Dim operators As String() = {"<=", ">=", "<", ">", "="}
+        For Each op As String In operators
+            If constraintPart.Contains(op) Then
+                Dim valueText As String = constraintPart.Split(op)(1)
+                If Double.TryParse(valueText, constraintValue) Then
+                    Return constraintValue
+                End If
+            End If
+        Next
+
+        Return 0 ' Si no se encuentra un valor válido, devolver 0
+    End Function
+
+    ' Evento que se ejecuta cuando se cambia el valor de alfa/beta
+    Private Sub txtAlphaBeta_TextChanged(sender As Object, e As EventArgs) Handles txtAlphaBeta.TextChanged
+        ' Calcular EQD2 y BED del órgano basado en el valor de alfa/beta ingresado
+        CalcularEQD2yBEDOrgano()
+    End Sub
+
+    ' Función para calcular EQD2 y BED del órgano basado en el valor de alfa/beta
+    Private Sub CalcularEQD2yBEDOrgano()
+        ' Verificar si se ha ingresado un valor válido para alfa/beta
+        If Not Double.TryParse(txtAlphaBeta.Text, Nothing) Then
+            txtEQD2Organo.Text = "0.00"
+            txtBEDOrgano.Text = "0.00"
+            Return
+        End If
+
+        ' Obtener el valor de alfa/beta
+        Dim alphaBetaRatio As Double = Double.Parse(txtAlphaBeta.Text)
+
+        ' Obtener el valor de "Constraint" del órgano seleccionado
+        Dim constraint As Double
+        If Double.TryParse(txtValorConstraint.Text, constraint) Then
+            ' Fórmulas para EQD2 y BED del órgano
+            Dim EQD2Organo As Double = constraint * (2 + alphaBetaRatio) / (2 + alphaBetaRatio)
+            Dim BEDOrgano As Double = constraint * (1 + 2 / alphaBetaRatio)
+
+            ' Mostrar los valores calculados en los TextBox correspondientes
+            txtEQD2Organo.Text = EQD2Organo.ToString("F2")
+            txtBEDOrgano.Text = BEDOrgano.ToString("F2")
+        Else
+            txtEQD2Organo.Text = "0.00"
+            txtBEDOrgano.Text = "0.00"
+        End If
+    End Sub
+
+    ' Evento que se ejecuta cuando se hace clic en el botón Calcular
+    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        ' Verificar si se han ingresado valores válidos
+        If Not Double.TryParse(txtDosisFisica.Text, Nothing) Or Not Integer.TryParse(txtSesiones.Text, Nothing) Or Not Double.TryParse(txtAlphaBeta.Text, Nothing) Then
+            MessageBox.Show("Por favor, ingrese valores válidos para la dosis física, número de sesiones y alfa/beta.")
+            Return
+        End If
+
+        ' Obtener los valores ingresados por el usuario
+        Dim dosisFisica As Double = Double.Parse(txtDosisFisica.Text)
+        Dim sesiones As Integer = Integer.Parse(txtSesiones.Text)
+        Dim alphaBetaRatio As Double = Double.Parse(txtAlphaBeta.Text)
+
+        ' Calcular la dosis por fracción
+        Dim dosisPorFraccion As Double = dosisFisica / sesiones
+
+        ' Calcular EQD2 y BED
+        Dim EQD2 As Double = dosisPorFraccion * (dosisPorFraccion + alphaBetaRatio) / (2 + alphaBetaRatio) * sesiones
+        Dim BED As Double = dosisPorFraccion * sesiones * (1 + dosisPorFraccion / alphaBetaRatio)
+
+        ' Mostrar los valores calculados en los TextBox correspondientes
+        txtEQD2Calculado.Text = EQD2.ToString("F2")
+        txtBEDCalculado.Text = BED.ToString("F2")
+    End Sub
+End Class
